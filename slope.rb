bb0700b21ed9b05eb1a1463daad3cd885114f49e
Leora <lw51685@students.westport.k12.ct.us>
2015-02-06 11:26:48 -0500

    I finished the slope calculator

--- slope.rb
+++ slope.rb
@@ -1,1 +1,15 @@
-#
+# get coordinates of points
+puts "x coordinate of point one?"
+x1 = gets.to_i
+puts "y coordinate of point one?"
+y1 = gets.to_i
+puts "x coordinate of point two?"
+x2 = gets.to_i
+puts "y coordinate of point two?"
+y2 = gets.to_i
+# calculate slope
+slope = (y2 - y1) / (x2 - x1)
+# output result to user
+puts "Slope:"
+puts slope
+# finished
